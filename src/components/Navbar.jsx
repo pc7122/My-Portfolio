d6ed381e1ed1fa62f2721392f6c08fbf3da3bd90
Prathamesh Chaudhary <prathameshchaudhary7122@gmail.com>
2024-03-07 23:37:28 +0530

    framer effects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 'use client'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
+import { stagger, useAnimate } from 'framer-motion';
 import Toggle from './Toggle.jsx';
 
 export default function Header() {
     const [showNav, setShowNav] = useState(false);
+    const [scope, animate] = useAnimate();
+
 
     const navItems = [
         { name: 'Home', link: '/' },
@@ -13,26 +16,56 @@ export default function Header() {
         { name: 'Photography', link: '/photographs' },
     ];
 
+    useEffect(() => {
+        if (window.innerWidth > 768) setShowNav(true);
+    }, []);
+
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 768) setShowNav(true);
+        else setShowNav(false);
+    });
+
+    useEffect(() => {
+        animate("nav", {
+            clipPath: showNav ? "circle(200% at top 0 right 50%)" : "circle(0% at top 0 right 50%)",
+        }, {
+            duration: 0.5,
+            transition: { type: "spring", stiffness: 400, damping: 40 }
+        });
+
+        animate("li",
+            showNav
+                ? { opacity: 1, y: 0 }
+                : { opacity: 0, y: 20 },
+            {
+                duration: 0.2,
+                delay: showNav ? stagger(0.1, { startDelay: 0.15 }) : 0,
+            });
+
+    }, [showNav, animate]);
+
     const toggleNav = () => {
         setShowNav(!showNav);
     }
 
     return (
-        <header className="fixed top-0 left-0 right-0 w-full bg-white dark:bg-black z-10">
+        <header ref={scope} className="fixed top-0 left-0 right-0 w-full bg-white dark:bg-black z-10">
             <div className="container mx-auto p-4 pt-6">
                 <div className="flex flex-wrap justify-between items-center">
                     <div>
-                        <h1 className="text-2xl font-bold leading-tight">My Port<span className="text-gray-900 dark:text-white">folio</span></h1>
+                        <h1 className="text-2xl font-bold leading-tight text-transparent bg-clip-text bg-gradient-to-br blue-shade dark:yellow-shade">
+                            My Port<span className='text-gray-950 dark:text-white'>folio</span>
+                        </h1>
                     </div>
 
-                    <nav className={`absolute md:static left-0 top-[5rem] md:top-0 w-full md:w-fit flex justify-center bg-white dark:bg-black transition-all md:translate-y-0 md:scale-y-100 ${showNav ? 'translate-y-0 scale-y-100' : '-translate-y-1/2 scale-y-0'}`} >
-                        <ul className="flex md:flex-row flex-col text-center gap-6 md:gap-8 leading-normal pb-10 md:pb-0">
+                    <nav className="absolute md:static left-0 top-[5rem] md:top-0 w-full md:w-fit flex justify-center bg-white dark:bg-black shadow md:shadow-none">
+                        <ul className="flex md:flex-row flex-col text-center gap-6 md:gap-8 leading-normal py-6 md:py-0">
                             {navItems.map((item, index) => (
                                 <li key={index} className="overflow-hidden">
                                     <Link href={item.link} className="text-gray-950 dark:text-gray-300 text-base font-bold">
                                         <span
                                             data-hover={item.name}
-                                            className="relative transition-all inline-block before:content-[attr(data-hover)] before:text-yellow-400 before:absolute before:top-full hover:-translate-y-full">
+                                            className="relative transition-all inline-block before:content-[attr(data-hover)] before:text-blue-700 dark:before:text-yellow-400 before:absolute before:top-full hover:-translate-y-full">
                                             {item.name}
                                         </span>
                                     </Link>
